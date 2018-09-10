488ff08cf803b564fe2ab6ef2a36298f920f12a5
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-09-10 08:39:48 -0500

    clean up identification and handling of unavailable product ids

--- api/providers/production/production_provider.py
+++ api/providers/production/production_provider.py
@@ -419,12 +419,14 @@ class ProductionProvider(ProductionProviderInterfaceV0):
 
         if non_plot_ids:
             urls = dict()
+            unavailable, available = list(), list()
             token = inventory.get_session()
             for dataset, ids in inventory.split_by_dataset(non_plot_ids).items():
                 try:
-                    verified    = inventory.verify_scenes(token, ids, dataset)
-                    unavailable = [k for k, v in verified.item() if not v]
-                    available   = [k for k, v in verified.items() if v]
+                    verified = inventory.verify_scenes(token, ids, dataset)
+                    # {'LT04_L1TP_007057_19871226_20170210_01_T1': True, 'LT04_L1TP_007057_19880111_20170210_01_T1': False, ...}
+                    for _id, _availability in verified.items():
+                        available.append(_id) if _availability else unavailable.append(_id)
                     if unavailable:
                         unavailable_scenes = Scene.find(unavailable)
                         self.set_products_unavailable(unavailable_scenes, "Scene no longer available")
