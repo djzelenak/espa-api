d3b46574ab0f97bff80877368623383f39b660f3
danzelenak-usgs <dzelenak@contractor.usgs.gov>
2019-12-18 14:23:24 -0600

    updates for caching provider

--- api/providers/caching/caching_provider.py
+++ api/providers/caching/caching_provider.py
@@ -1,8 +1,6 @@
 import os
-
-from api.providers.caching import CachingProviderInterfaceV0
-
 import memcache
+from api.providers.caching import CachingProviderInterfaceV0
 
 
 class CachingProviderException(Exception):
@@ -15,7 +13,7 @@ class CachingProvider(CachingProviderInterfaceV0):
         if not memcache_hosts:
             memcache_hosts = os.getenv('ESPA_MEMCACHE_HOST', '127.0.0.1:11211').split(',')
         self.cache = memcache.Client(memcache_hosts, debug=debug)
-        self.timeout = timeout # seconds
+        self.timeout = timeout  # seconds
 
     def get(self, cache_key):
         return self.cache.get(cache_key)
